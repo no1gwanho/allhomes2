1ed25340d7624f13763fae0c115d4b94ba20211d
bluebeen <68265438+bluebeen@users.noreply.github.com>
2020-11-26 08:40:15 +0900

    store controller edit

--- AllHomesTest/src/main/java/com/allhomes/myapp/store/StoreController.java
+++ AllHomesTest/src/main/java/com/allhomes/myapp/store/StoreController.java
@@ -1,15 +1,31 @@
 package com.allhomes.myapp.store;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StoreController {
+	@Autowired
+	SqlSession sqlSes;
+	@Autowired
+	DataSourceTransactionManager transactionManager;
 	
 	@RequestMapping("/storeHome")
-	public String storeHome() {
-		return "store/storeHome";		
+	public ModelAndView storeHome() {
+		ProductDaoImp dao = sqlSes.getMapper(ProductDaoImp.class);
+		List<ProductVO> list = dao.productAllList();
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("list", list);
+		mav.setViewName("store/storeHome");
+		
+		return mav;		
 	}
 	
 	@RequestMapping("/storeCategory")
@@ -25,10 +41,14 @@ public class StoreController {
 	}
 	
 	@RequestMapping("/storeDetail")
-	public String storeDetail(){
+	public ModelAndView storeDetail(int pd_no){
+		ProductDaoImp dao = sqlSes.getMapper(ProductDaoImp.class);
+		ProductVO vo = dao.selectProduct(pd_no);
 		
-		return "store/storeDetail";
-	}
-	
-	
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("vo", vo);
+		mav.setViewName("store/storeDetail");
+		
+		return mav;
+	}	
 }
