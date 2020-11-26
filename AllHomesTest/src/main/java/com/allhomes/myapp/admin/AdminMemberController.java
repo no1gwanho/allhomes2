4ae71e58fdfd6604b703ec7ec02eacf800f5afdd
serank <kseran1002@gmail.com>
2020-11-26 20:47:04 +0900

    update: RegisterMapper, upload/mypage 폴더 추가

--- AllHomesTest/src/main/java/com/allhomes/myapp/admin/AdminMemberController.java
+++ AllHomesTest/src/main/java/com/allhomes/myapp/admin/AdminMemberController.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.allhomes.myapp.register.RegisterDaoImp;
@@ -26,7 +27,6 @@ public class AdminMemberController {
 	//회원관리 페이지로 이동
 	@RequestMapping("/adminMemberMain")
 	public ModelAndView adminMember() {
-		
 		RegisterDaoImp dao = sqlSession.getMapper(RegisterDaoImp.class);
 		List<RegisterVO> list = dao.memberAllSelect();
 		
@@ -38,10 +38,30 @@ public class AdminMemberController {
 		return mav;
 	}
 		
+	//회원관리 페이지 정렬하기
+	@RequestMapping("/adminMemberMainOrder")
+	public ModelAndView adminMemberMainOrder(@RequestParam("val") String val) {
+		RegisterDaoImp dao = sqlSession.getMapper(RegisterDaoImp.class);
+		List<RegisterVO> list = dao.memberAllSelectOrder(val);
+		
+		ModelAndView mav = new ModelAndView();
+		
+		System.out.println("val============="+val);
+		
+		mav.addObject("list", list);
+		mav.setViewName("admin/adminMember/adminMemberMain");
+		return mav;
+	}
 	
 	//회원관리 회원정보 상세 페이지로 이동
 	@RequestMapping("/adminMemberDetail")
-	public String MemberDetail() {
-		return "admin/adminMember/adminMemberDetail";
+	public ModelAndView MemberDetail(@RequestParam("m_no") int m_no) {
+		RegisterDaoImp dao = sqlSession.getMapper(RegisterDaoImp.class);
+		RegisterVO mVo = dao.memberSelect(m_no);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("mVo", mVo);
+		mav.setViewName("admin/adminMember/adminMemberDetail");
+		return mav;
 	}
 }
