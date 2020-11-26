0307390bd570f44a251787563a511194d60c27ec
serank <kseran1002@gmail.com>
2020-11-26 19:14:41 +0900

    add:AdminMemberController, UPDATE:RegisterVO 수정

--- AllHomesTest/src/main/java/com/allhomes/myapp/register/RegisterDaoImp.java
+++ AllHomesTest/src/main/java/com/allhomes/myapp/register/RegisterDaoImp.java
@@ -1,10 +1,15 @@
 package com.allhomes.myapp.register;
 
+import java.util.List;
+
 public interface RegisterDaoImp {
 	
+	//로그인
 	public RegisterVO login(RegisterVO vo);
-	
 	public int customerInsert(RegisterVO vo);
+	
+	//전체 회원 조회
+	public List<RegisterVO> memberAllSelect();
 	
 
 }
