34c6b8ee4925d15f2745227c0bad61aeb1c72508
bluebeen <68265438+bluebeen@users.noreply.github.com>
2020-11-30 10:29:38 +0900

    cart error complete

--- AllHomesTest/src/main/java/com/allhomes/myapp/review/ReviewVO.java
+++ AllHomesTest/src/main/java/com/allhomes/myapp/review/ReviewVO.java
@@ -1,5 +1,53 @@
 package com.allhomes.myapp.review;
 
 public class ReviewVO {
-
+	private String userid;
+	private int pd_no;
+	private int rating;
+	private String img;
+	private String content;
+	private String writedate;
+	private int s_no;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public int getPd_no() {
+		return pd_no;
+	}
+	public void setPd_no(int pd_no) {
+		this.pd_no = pd_no;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWritedate() {
+		return writedate;
+	}
+	public void setWritedate(String writedate) {
+		this.writedate = writedate;
+	}
+	public int getS_no() {
+		return s_no;
+	}
+	public void setS_no(int s_no) {
+		this.s_no = s_no;
+	}
 }
