ff4e281362e075b60a66080abbffa6dfb2fb548e
bluebeen <68265438+bluebeen@users.noreply.github.com>
2020-12-10 17:49:47 +0900

    cart insert update

--- AllHomesTest/src/main/java/com/allhomes/myapp/cart/CartDaoImp.java
+++ AllHomesTest/src/main/java/com/allhomes/myapp/cart/CartDaoImp.java
@@ -6,13 +6,11 @@ public interface CartDaoImp {
 
 	public List<CartVO> allCartList();
 	
-	public CartVO selectCartList();
-	
-	public int addCartList(int pd_no);
+	public int addCartList(CartVO vo);
 	
 	public int editCartList(CartVO vo);
 	
-	public int delCartList(int pd_no, int m_no);
-
-	public List<CartJoinVO> joinCart(int pd_no);
+	public int delCartList(int pd_no);
+	
+	public List<CartJoinVO> joinCart(String userid);
 }
