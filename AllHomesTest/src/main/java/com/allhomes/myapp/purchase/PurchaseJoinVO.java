3c42407057f6b9189df292c90fc4bc7acc481f85
bluebeen <68265438+bluebeen@users.noreply.github.com>
2020-12-10 19:48:20 +0900

    order updating

---
+++ AllHomesTest/src/main/java/com/allhomes/myapp/purchase/PurchaseJoinVO.java
@@ -0,0 +1,233 @@
+package com.allhomes.myapp.purchase;
+
+public class PurchaseJoinVO {
+	private int pc_no;
+	private int pd_no;
+	private int s_no;
+	private String userid;
+	private int num;
+	private int price;
+	private int shipping_c;
+	private int total_p;
+	private int a_code;
+	private String pc_date;
+	private String payment;
+	private String status;
+	private String confirm;
+	private String memo;
+	private String o_value;
+	
+	private int rating;
+	private String img;
+	private String content;
+	private String writedate;
+	
+	private int m_no;
+	private String receiver;
+	private int zipcode;
+	private String addr;
+	private String addrdetail;
+	private String tel;
+
+	public int getPc_no() {
+		return pc_no;
+	}
+
+	public void setPc_no(int pc_no) {
+		this.pc_no = pc_no;
+	}
+
+	public int getPd_no() {
+		return pd_no;
+	}
+
+	public void setPd_no(int pd_no) {
+		this.pd_no = pd_no;
+	}
+
+	public int getS_no() {
+		return s_no;
+	}
+
+	public void setS_no(int s_no) {
+		this.s_no = s_no;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getShipping_c() {
+		return shipping_c;
+	}
+
+	public void setShipping_c(int shipping_c) {
+		this.shipping_c = shipping_c;
+	}
+
+	public int getTotal_p() {
+		return total_p;
+	}
+
+	public void setTotal_p(int total_p) {
+		this.total_p = total_p;
+	}
+
+	public int getA_code() {
+		return a_code;
+	}
+
+	public void setA_code(int a_code) {
+		this.a_code = a_code;
+	}
+
+	public String getPc_date() {
+		return pc_date;
+	}
+
+	public void setPc_date(String pc_date) {
+		this.pc_date = pc_date;
+	}
+
+	public String getPayment() {
+		return payment;
+	}
+
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public String getO_value() {
+		return o_value;
+	}
+
+	public void setO_value(String o_value) {
+		this.o_value = o_value;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWritedate() {
+		return writedate;
+	}
+
+	public void setWritedate(String writedate) {
+		this.writedate = writedate;
+	}
+
+	public int getM_no() {
+		return m_no;
+	}
+
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getAddrdetail() {
+		return addrdetail;
+	}
+
+	public void setAddrdetail(String addrdetail) {
+		this.addrdetail = addrdetail;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+}
--- AllHomesTest/src/main/webapp/WEB-INF/views/cart/cartForm.jsp
+++ AllHomesTest/src/main/webapp/WEB-INF/views/cart/cartForm.jsp
@@ -1,36 +1,49 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<script>
-$(document).ready(function(){
-    //최상단 체크박스 클릭
-    $("#checkall").click(function(){
-        //클릭되었으면
-        if($("#checkall").prop("checked")){
-            //input태그의 name이 chk인 태그들을 찾아서 checked옵션을 true로 정의
-            $("input[type=checkbox]").prop("checked",true);
-            //클릭이 안되있으면
-        }else{
-            //input태그의 name이 chk인 태그들을 찾아서 checked옵션을 false로 정의
-            $("input[type=checkbox]").prop("checked",false);
-        }
-    })
-})
-</script>
 <br/>
 <div class="container">
 <h2>장바구니</h2>
 	<div class="row">
 		<div class="col-md-7">
-			<input type ="checkbox" id="checkAll" style="background-color:#ee8374;"/>&nbsp;모두 체크
+			<input type ="checkbox" name="allCheck" id="allCheck" style="background-color:#ee8374;"/><label for = "allCheck">모두 선택</label>
+			<script>
+				$("#allCheck").click(function(){
+					var chk = $("#allCheck").prop("checked");
+					if(chk){
+						$(".chBox").prop("checked", true);
+					}else{
+						$(".chBox").prop("checked", false);
+					}
+				});
+			</script>
 		</div>
-		<div class="col-md-2"><button class="btn btn" style="font-size:0.8em;background-color:#ee8374;color:#fff;border:0;">선택삭제</button></div>
+		<div class="col-md-2">
+			<button class="btn btn" id="selectDelBtn" style="font-size:0.8em;background-color:#ee8374;color:#fff;border:0;">선택삭제</button>
+			<script>
+				$("#selectDelBtn").click(function(){
+					var confirm_val= confir("정말 삭제하시겠습니까?");
+					
+					if(confirm_val){
+						var checkArr = new Array();
+						$("input[class='chBox']:checked").each(function(){
+							
+						});
+					}
+				});
+			</script>
+		</div>
 	</div>
 	<br/>
 	<form method="post" action="/myapp/order?userid=${c.userid }">
 		<c:forEach var="c" items="${list }">
 			<div class="row" style="margin-top:15px;">
 				<div class="col-3">
-					<input type="checkbox" name="chk">
+					<input type="checkbox" name="chBox" class="chBox">
+					<script>
+						$(".chBox").click(function(){
+							$("#allCheck").prop("checked", false);
+						});
+					</script>
 					<img style="maring-left:15px;width:150px; height:120px;" src="<%=request.getContextPath()%>/resources/upload/productMainImg/${c.s_no}/${c.main_img}"/>
 				</div>
 				<div class="col-9" style="margin-left:15px;text-align:center">
