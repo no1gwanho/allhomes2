8e9cbec74597562f0f85e70928a75a7bdfb2fbf2
serank <kseran1002@gmail.com>
2020-12-17 06:36:18 +0900

    update

--- AllHomesTest/src/main/java/com/allhomes/myapp/admin/AdminMemberController.java
+++ AllHomesTest/src/main/java/com/allhomes/myapp/admin/AdminMemberController.java
@@ -16,165 +16,180 @@ import com.allhomes.myapp.register.RegisterVO;
 @Controller
 public class AdminMemberController {
 	SqlSession sqlSession;
+
 	public SqlSession getSqlSession() {
 		return sqlSession;
 	}
+
 	@Autowired
 	public void setSqlSession(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
 	}
-	
-	
-	//회원관리 페이지로 이동(메인)
+
+	// 회원관리 페이지로 이동(메인)
 	@RequestMapping("/adminMemberMain")
 	public ModelAndView adminMember() {
 		AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
-		
+
 		List<RegisterVO> list = dao.memberSelectMain();
-		
-		
-		//count register
+
+		// count register
 		int month = dao.countRegisterMonth();
 		int allCnt = dao.countRegisterTotal();
 		int today = dao.countRegisterToday();
-		
+
 		ModelAndView mav = new ModelAndView();
-		
-		//일주일 간 가입한 회원 수
-		for(int i=0; i<7; i++) {
-			mav.addObject("count"+i,dao.countRegisMain(i));
+
+		// 일주일 간 가입한 회원 수
+		for (int i = 0; i < 7; i++) {
+			mav.addObject("count" + i, dao.countRegisMain(i));
 		}
-		
+
 		mav.addObject("out", dao.countRegisOut());
 		mav.addObject("month", month);
 		mav.addObject("allCnt", allCnt);
 		mav.addObject("today", today);
 		mav.addObject("list", list);
-		
+
 		mav.setViewName("admin/adminMember/adminMemberMain");
 		return mav;
 	}
-		
+
+	// 회원정보 상세 페이지로 이동
+	/*
+	 * @RequestMapping("/adminMemberDetail") public ModelAndView
+	 * MemberDetail(@RequestParam("m_no") int m_no, AdminPagingVO vo
+	 * , @RequestParam(value="nowPage", required=false)String nowPage
+	 * , @RequestParam(value="cntPerPage", required=false)String cntPerPage) {
+	 * 
+	 * AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
+	 * RegisterVO mVo = dao.memberSelect(m_no); //회원 정보
+	 * 
+	 * 
+	 * // paging// int total = dao.homeboardCnt(mVo.getUserid()); // 총 집들이게시판 수 if
+	 * (nowPage == null && cntPerPage == null) { nowPage = "1"; cntPerPage = "10"; }
+	 * else if (nowPage == null) { nowPage = "1"; } else if (cntPerPage == null) {
+	 * cntPerPage = "10"; } // paging//
+	 * 
+	 * ModelAndView mav = new ModelAndView(); mav.addObject("mVo", mVo);
+	 * 
+	 * //집들이 페이징 vo = new AdminPagingVO(total, Integer.parseInt(nowPage),
+	 * Integer.parseInt(cntPerPage)); mav.addObject("HBpaging", vo); //집들이 리스트
+	 * mav.addObject("hbList",dao.memberHomeboard(vo));
+	 * 
+	 * 
+	 * //주문 페이징
+	 * 
+	 * total = dao.orderCnt(mVo.getUserid()); vo = new AdminPagingVO(total,
+	 * Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
+	 * mav.addObject("orderpaging", vo);
+	 * 
+	 * //주문 리스트 List<AdminOrderVO> oList = dao.memberPurchase(mVo.getUserid());
+	 * //주문내역
+	 * 
+	 * 
+	 * //집들이 게시판
+	 * 
+	 * //회원 정보 AdminMemberInfoCountVO infoVO = new AdminMemberInfoCountVO();
+	 * infoVO.setBoardCnt(dao.memberHBCnt(mVo.getUserid())+dao.memberQnaCnt(mVo.
+	 * getUserid())+ dao.memberReviewCnt(m_no));
+	 * infoVO.setOrderCnt(dao.memberOrderCnt(mVo.getUserid()));
+	 * infoVO.setScrapCnt(dao.memberScrapCnt(m_no));
+	 * infoVO.setWishCnt(dao.memberWishCnt(dao.memberSelect(m_no).getUserid()));
+	 * 
+	 * 
+	 * mav.addObject("infoVO", infoVO);//회원 정보 //mav.addObject("oList", oList);
+	 * //주문내역 mav.setViewName("admin/adminMember/adminMemberDetail"); return mav; }
+	 */
+
 	
 	
-	//회원정보 상세 페이지로 이동
-	@RequestMapping("/adminMemberDetail")
-	public ModelAndView MemberDetail(@RequestParam("m_no") int m_no) {
-		AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
-		RegisterVO mVo = dao.memberSelect(m_no); //회원 정보
-		
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("mVo", mVo);
-		
-		
-		AdminMemberInfoCountVO infoVO = new AdminMemberInfoCountVO();
-		infoVO.setBoardCnt(dao.memberHBCnt(mVo.getUserid())+dao.memberQnaCnt(mVo.getUserid())+ dao.memberReviewCnt(m_no));
-		infoVO.setOrderCnt(dao.memberOrderCnt(mVo.getUserid()));
-		infoVO.setScrapCnt(dao.memberScrapCnt(m_no));
-		infoVO.setWishCnt(dao.memberWishCnt(m_no));
-		
-		List<AdminOrderVO> oList = dao.memberPurchase(mVo.getUserid()); //주문내역
-		
-		mav.addObject("infoVO", infoVO);
-		mav.addObject("oList", oList);
-		mav.setViewName("admin/adminMember/adminMemberDetail");
-		return mav;
-	}
-	
-	//멤버리스트 페이지로 이동
+	// 멤버리스트 페이지로 이동
 	@RequestMapping("/adminMemberList")
-	public ModelAndView memberList(AdminPagingVO vo
-			, @RequestParam(value="nowPage", required=false)String nowPage
-			, @RequestParam(value="cntPerPage", required=false)String cntPerPage) {
-		
+	public ModelAndView memberList(AdminPagingVO vo, @RequestParam(value = "nowPage", required = false) String nowPage,
+			@RequestParam(value = "cntPerPage", required = false) String cntPerPage) {
+
 		AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
-		
-		
-		//paging//
-		int total = dao.countRegisterTotal(); //총회원수
+
+		// paging//
+		int total = dao.countRegisterTotal(); // 총회원수
 		if (nowPage == null && cntPerPage == null) {
 			nowPage = "1";
 			cntPerPage = "15";
 		} else if (nowPage == null) {
 			nowPage = "1";
-		} else if (cntPerPage == null) { 
+		} else if (cntPerPage == null) {
 			cntPerPage = "15";
 		}
 		vo = new AdminPagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
-		//paging//
-		
+		// paging//
+
 		ModelAndView mav = new ModelAndView();
-		
-		//paging
+
+		// paging
 		mav.addObject("paging", vo);
 		mav.addObject("viewAll", dao.memberAllSelect(vo));
-		
+
 		mav.setViewName("admin/adminMember/adminMemberList");
 		return mav;
 	}
-	
-	//멤버리스트 페이지로 이동 (정렬)
-		@RequestMapping("/adminMemberOrder")
-		public ModelAndView memberOrder(@RequestParam("order") String order,
-										AdminPagingVO vo
-										, @RequestParam(value="nowPage", required=false)String nowPage
-										, @RequestParam(value="cntPerPage", required=false)String cntPerPage) {
-			
-			AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
-			
-			//paging//
-			int total = dao.countRegisterTotal(); //총회원수
-			if (nowPage == null && cntPerPage == null) {
-				nowPage = "1";
-				cntPerPage = "15";
-			} else if (nowPage == null) {
-				nowPage = "1";
-			} else if (cntPerPage == null) { 
-				cntPerPage = "15";
-			}
-			vo = new AdminPagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
-			
-	
-			ModelAndView mav = new ModelAndView();
-			//paging
-			mav.addObject("paging", vo);
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("order", order);
-			map.put("start", vo.getStart());
-			map.put("end", vo.getEnd());
-			mav.addObject("viewAll", dao.memberAllSelectOrder(map));
-			
-			mav.setViewName("admin/adminMember/adminMemberList");
-			return mav;
-			
-		}
-		
-	//회원 검색(선택검색)
-	@RequestMapping("/adminMemberSearch")
-	public ModelAndView memberSearch(@RequestParam("value") String value
-									,@RequestParam("key") String key
-									,AdminPagingVO vo
-									, @RequestParam(value="nowPage", required=false)String nowPage
-									, @RequestParam(value="cntPerPage", required=false)String cntPerPage) {
-		
-		
+
+	// 멤버리스트 페이지로 이동 (정렬)
+	@RequestMapping("/adminMemberOrder")
+	public ModelAndView memberOrder(@RequestParam("order") String order, AdminPagingVO vo,
+			@RequestParam(value = "nowPage", required = false) String nowPage,
+			@RequestParam(value = "cntPerPage", required = false) String cntPerPage) {
+
 		AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
-		
-		//paging//
-		int total = dao.countRegisterTotal(); //총회원수
+
+		// paging//
+		int total = dao.countRegisterTotal(); // 총회원수
 		if (nowPage == null && cntPerPage == null) {
 			nowPage = "1";
 			cntPerPage = "15";
 		} else if (nowPage == null) {
 			nowPage = "1";
-		} else if (cntPerPage == null) { 
+		} else if (cntPerPage == null) {
 			cntPerPage = "15";
 		}
 		vo = new AdminPagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
-		
 
 		ModelAndView mav = new ModelAndView();
-		//paging
+		// paging
+		mav.addObject("paging", vo);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("order", order);
+		map.put("start", vo.getStart());
+		map.put("end", vo.getEnd());
+		mav.addObject("viewAll", dao.memberAllSelectOrder(map));
+
+		mav.setViewName("admin/adminMember/adminMemberList");
+		return mav;
+
+	}
+
+	// 회원 검색(선택검색)
+	@RequestMapping("/adminMemberSearch")
+	public ModelAndView memberSearch(@RequestParam("value") String value, @RequestParam("key") String key,
+			AdminPagingVO vo, @RequestParam(value = "nowPage", required = false) String nowPage,
+			@RequestParam(value = "cntPerPage", required = false) String cntPerPage) {
+
+		AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
+
+		// paging//
+		int total = dao.countRegisterTotal(); // 총회원수
+		if (nowPage == null && cntPerPage == null) {
+			nowPage = "1";
+			cntPerPage = "15";
+		} else if (nowPage == null) {
+			nowPage = "1";
+		} else if (cntPerPage == null) {
+			cntPerPage = "15";
+		}
+		vo = new AdminPagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
+
+		ModelAndView mav = new ModelAndView();
+		// paging
 		mav.addObject("paging", vo);
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("value", value);
@@ -182,53 +197,51 @@ public class AdminMemberController {
 		map.put("start", vo.getStart());
 		map.put("end", vo.getEnd());
 		mav.addObject("viewAll", dao.memberSearch(map));
-		
+
 		mav.setViewName("admin/adminMember/adminMemberList");
 		return mav;
-		
+
 	}
-	
-	//회원 상세 검색 
+
+	// 회원 상세 검색
 	@RequestMapping("/MemberDetailSearch")
-	public ModelAndView memberDeatilSearch(RegisterDetailSearchVO regVo
-									,AdminPagingVO vo
-									, @RequestParam(value="nowPage", required=false)String nowPage
-									, @RequestParam(value="cntPerPage", required=false)String cntPerPage) {
+	public ModelAndView memberDeatilSearch(RegisterDetailSearchVO regVo, AdminPagingVO vo,
+			@RequestParam(value = "nowPage", required = false) String nowPage,
+			@RequestParam(value = "cntPerPage", required = false) String cntPerPage) {
 		AdminMemberDaoImp dao = sqlSession.getMapper(AdminMemberDaoImp.class);
-		
-		
-		//paging//
-		int total = dao.countRegisterTotal(); //총회원수
+
+		// paging//
+		int total = dao.countRegisterTotal(); // 총회원수
 		if (nowPage == null && cntPerPage == null) {
 			nowPage = "1";
 			cntPerPage = "15";
 		} else if (nowPage == null) {
 			nowPage = "1";
-		} else if (cntPerPage == null) { 
+		} else if (cntPerPage == null) {
 			cntPerPage = "15";
 		}
 		vo = new AdminPagingVO(total, Integer.parseInt(nowPage), Integer.parseInt(cntPerPage));
-		
-		System.out.println("a;ldskfadsf"+regVo.getDate()+regVo.getDate2()+regVo.getUsername());
-		
+
+		System.out.println("a;ldskfadsf" + regVo.getDate() + regVo.getDate2() + regVo.getUsername());
+
 		ModelAndView mav = new ModelAndView();
-		//paging
+		// paging
 		mav.addObject("paging", vo);
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("userid", regVo.getUserid() );
+		map.put("userid", regVo.getUserid());
 		map.put("nickname", regVo.getNickname());
 		map.put("tel", regVo.getTel());
 		map.put("email", regVo.getEmail());
 		map.put("date", regVo.getDate());
 		map.put("date2", regVo.getDate2());
-		
+
 		map.put("start", vo.getStart());
 		map.put("end", vo.getEnd());
 		mav.addObject("viewAll", dao.memberSearchDetail(map));
-		
+
 		mav.setViewName("admin/adminMember/adminMemberList");
 		return mav;
-		
+
 	}
-	
+
 }
--- AllHomesTest/src/main/webapp/WEB-INF/views/admin/adminMember/adminMemberMain.jsp
+++ AllHomesTest/src/main/webapp/WEB-INF/views/admin/adminMember/adminMemberMain.jsp
@@ -8,7 +8,7 @@ td {
 	line-height: 50px;
 }
 #memberChart {
-	height: 500px;
+	height: 505px;
 }
 .cardSize {
 	height: 240px;
@@ -96,15 +96,14 @@ td {
 				<div class="card border-left-info shadow h-100 py-2">
 					<div class="card-body">
 						<div class="row no-gutters align-items-center">
-							<div class="col-auto">
-								<div class="h3 font-weight-bold text-info text-uppercase mb-1"
-									style="float: right">Today</div>
-								<i class="fas fa-user fa-3x text-gray-300"></i>
+							<div class="col mr-2">
+								<div
+									class="text-xl font-weight-bold text-primary text-uppercase mb-1">
+									Today</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">+${today}</div>
 							</div>
-							<div class="col-lg-12 mr-2">
-								<br />
-								<br />
-								<div class="h1 mb-0 font-weight-bold text-gray-600">&nbsp;&nbsp;&nbsp;+${today}</div>
+							<div class="col-auto">
+								<i class="fas fa-users fa-2x text-gray-300"></i>
 							</div>
 						</div>
 					</div>
